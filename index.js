bab1a7298a1e996c423fdfbe5c4043aad27b4b7d
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-09-08 18:53:05 +0100

    in a last effort to create a change, i must trying doing something different

--- index.js
+++ index.js
@@ -26,9 +26,8 @@ io.on('connection', (socket) => {
           userId,
           uname
         })
-        
-        socket.to(roomId).except(socket.id).emit("joined", userId);
         io.to(roomId).emit("updateP", rooms[roomId]);
+        socket.to(roomId).except(socket.id).emit("joined", userId);
         socket.on("chat", (data, uname)=>{
           socket.to(roomId).emit('data', data, uname)
         });
