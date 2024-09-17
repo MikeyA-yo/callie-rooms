e49fc2226badf18b9ed9eb67ceb6623a12c4596b
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-09-17 09:53:46 +0100

    add another ev

--- index.js
+++ index.js
@@ -54,6 +54,11 @@ io.on('connection', (socket) => {
             io.to(roomId).emit("updateP", rooms[roomId]);
             socket.to(roomId).except(socket.id).emit('user-disconnected', userId, rooms[roomId])
         });
+        socket.on("leave", () =>{
+            rooms[roomId] = rooms[roomId].filter(user => user.userId !== userId);
+            io.to(roomId).emit("updateP", rooms[roomId]);
+            socket.to(roomId).except(socket.id).emit('user-disconnected', userId, rooms[roomId])
+        })
         socket.on("end", ()=>{
             socket.leave(roomId)
             rooms[roomId] = []
