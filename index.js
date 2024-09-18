5de2cb358ffbdcaadc4a3b00152c0945869311bc
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-09-18 17:05:06 +0100

    basic test

--- index.js
+++ index.js
@@ -57,9 +57,10 @@ io.on('connection', (socket) => {
           io.to(roomId).emit("updateP", rooms[roomId]);
         })
         socket.on('disconnect', () => {
+            let uname = rooms[roomId].filter(user => user.userId === userId)[0].uname
             rooms[roomId] = rooms[roomId].filter(user => user.userId !== userId);
             io.to(roomId).emit("updateP", rooms[roomId]);
-            socket.to(roomId).except(socket.id).emit('user-disconnected', userId, rooms[roomId])
+            socket.to(roomId).except(socket.id).emit('user-disconnected', userId, rooms[roomId], uname)
         });
         socket.on("leave", () =>{
             rooms[roomId] = rooms[roomId].filter(user => user.userId !== userId);
